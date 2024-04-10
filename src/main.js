218459a1eca4896fd36062e7ab50576c94912911
KvashaVladyslav <146590289+KvashaVladyslav@users.noreply.github.com>
2024-04-10 18:53:30 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,3 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-import { apiGet } from "./js/api";
-// import { apiPost } from './js/api';
-apiGet();
-// apiPost()
