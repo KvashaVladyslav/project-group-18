7fc5531041bdffb98c8901dede41db2316d29154
KvashaVladyslav <146590289+KvashaVladyslav@users.noreply.github.com>
2024-04-11 17:39:49 +0200

    upp api and import main.js

--- src/js/api.js
+++ src/js/api.js
@@ -5,19 +5,21 @@ export async function apiGet(){
     const end_point = "/api/reviews"
     const url = `${base_url}${end_point}`
 
-    const res = await axios.get(url).then(response => response.json).catch(console.error());
-    
-    return res
+    const res = await axios.get(url)
+    return res.data
     
 }
 
-// export async function apiPost(){
-//     const base = "https://portfolio-js.b.goit.study"
-//     const end = "/api/requests"
-//     const url = `${base}${end}`
+export async function apiPost(email, phone) {
+    const base = "https://portfolio-js.b.goit.study";
+    const end = "/api/requests";
+    const url = `${base}${end}`;
 
-//     const res = await axios.post(url).then(response => response.json).catch(error => console.log(error));
-    
-//     return res
-    
-// }
+    try {
+        const response = await axios.post(url, { email, phone });
+        return response.data;
+    } catch (error) {
+        console.error("Error submitting request:", error);
+        throw error;
+    }
+}
