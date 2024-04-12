c605dfaf94da196bf87ae7ab2224e90167e2fb09
Maksym Topolya <148054104+MaksymTopolya@users.noreply.github.com>
2024-04-12 19:35:40 +0300

    footer/modal
    maks

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -1,30 +1,28 @@
-import { apiPost, apiGet } from "./api";
-import { openModal, closeModal } from "./modal-window";
+import { apiPost } from "./api";
+import modalWindow from "./modal-window";
 
 const form = document.querySelector(".form-section");
 
-form.addEventListener("submit", handleSubmitForm);
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-
-async function handleSubmitForm(event) {
-    event.preventDefault(); 
-    
-
-    const { email, number } = event.target.elements; 
-    console.log(email.value)
-    console.log(number.value)
-   
+    const { email, comment } = event.target.elements;
+    console.log(email.value);
+    console.log(comment.value);
 
     try {
-        const response = await apiPost(email.value, number.value); 
-        console.log(response)
+        const response = await apiPost(email.value, comment.value);
+        console.log(response);
         if (response) {
-            openModal(); 
-            form.reset(); 
-        } 
+            modalWindow.render(response.title, response.message);
+            modalWindow.open();
+            form.reset();
+        }
     } catch (error) {
         console.error("Error submitting request:", error);
-        form.reset(); 
+        form.reset();
     }
-}
+});
+
+modalWindow.bindEvents();
 
