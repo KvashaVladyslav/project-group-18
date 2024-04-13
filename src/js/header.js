42797b627149e5a74da91e64f730131267f03d98
Maksym Topolya <148054104+MaksymTopolya@users.noreply.github.com>
2024-04-13 14:22:41 +0300

    final
    das

--- src/js/header.js
+++ src/js/header.js
@@ -1,6 +1,16 @@
 const mobileWindow = document.querySelector(".modal-phone-window")
 const burgerIcon = document.querySelector(".open-mobile-menu-btn")
 const closeBtn = document.querySelector(".svg-close")
+const menu = document.querySelector(".header-nav-menu-link")
+const menuList = document.querySelector(".menu-list")
+
+menu.addEventListener("click", () => {
+    if (menuList.classList.contains("visually-hidden")) {
+        menuList.classList.remove("visually-hidden");
+    } else {
+        menuList.classList.add("visually-hidden");
+    }
+});
 
 burgerIcon.addEventListener("click", () => {
     mobileWindow.classList.remove("hidden")
@@ -9,3 +19,4 @@ burgerIcon.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
      mobileWindow.classList.add("hidden")
 })
+
