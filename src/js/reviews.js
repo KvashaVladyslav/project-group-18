3358d94258ea36b3ee0eae809eb0f15750154fdd
LikaDevelop <67103713+LikaDevelop@users.noreply.github.com>
2024-04-14 01:00:19 +0300

    swiper

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -13,7 +13,7 @@ import 'swiper/css/pagination';
 const list_html_review = document.querySelector(".review-list");
 const list_of_reviews = await apiGet();
 for (let i = 0; i < list_of_reviews.length; i++) {
-    const card_of_review = `<li class="review-card">
+    const card_of_review = `<li class="review-card swiper-slide">
                             <img src=${list_of_reviews[i].avatar_url} alt="avatar" class="img-avatar">
                             <h3 class="author-name"> ${list_of_reviews[i].author}</h3>
                             <p class="review-text">${list_of_reviews[i].review}</p>
