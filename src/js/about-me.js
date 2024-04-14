362b8032721096157c6079ed691db768d729a03d
OksanaLysynchuk <138123244+OksanaLysynchuk@users.noreply.github.com>
2024-04-14 19:51:08 +0300

    accordeon added

---
+++ src/js/about-me.js
@@ -0,0 +1,34 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+new Accordion('.about-me-title-container');
+
+const btns = document.querySelectorAll('.about-me-button-slider-down');
+btns.forEach(btn => btn.addEventListener('click', handleClick));
+function toggleList(button, list) {
+  const svgElement = button.querySelector('use');
+  const isHidden = list.classList.contains('visually-hidden');
+  list.classList.toggle('visually-hidden');
+  svgElement.setAttribute(
+    'href',
+    isHidden
+      ? '../img/icons.svg#icon-arrow-up'
+      : '../img/icons.svg#icon-arrow-down'
+  );
+}
+function handleClick(event) {
+  const currentBtn = event.currentTarget;
+  const closest = currentBtn.closest('.about-me-item');
+  if (closest.querySelector('.about-me-paragraph-container')) {
+    const aboutMeParagraphContainer = closest.querySelector(
+      '.about-me-paragraph-container'
+    );
+    toggleList(currentBtn, aboutMeParagraphContainer);
+  } else if (closest.querySelector('.role-list')) {
+    const roleList = closest.querySelector('.role-list');
+    toggleList(currentBtn, roleList);
+  } else if (closest.querySelector('.education-list')) {
+    const educationList = closest.querySelector('.education-list');
+    toggleList(currentBtn, educationList);
+  }
+}
