936b0297d44c2596d58fcdf326a9e42dd9710276
Yevhenii Belov <studybel@yahoo.com>
2024-04-14 16:27:50 +0200

    Final update

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,42 +1,31 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//   // Функція анімації
-//   function startMarquee() {
-//     const marquee = document.querySelector('.covers-item');
-//     marquee.style.animationPlayState = 'running';
-//   }
+document.addEventListener('DOMContentLoaded', event => {
+  const startAnimation = () => {
+    const items = document.querySelectorAll('.covers-item');
+    items.forEach(item => {
+      item.style.animationPlayState = 'running';
+    });
+  };
 
-//   function stopMarquee() {
-//     const marquee = document.querySelector('.covers-item');
-//     marquee.style.animationPlayState = 'paused';
-//   }
+  const stopAnimation = () => {
+    const items = document.querySelectorAll('.covers-item');
+    items.forEach(item => {
+      item.style.animationPlayState = 'paused';
+    });
+  };
 
-//   let observer = new IntersectionObserver((entries, observer) => {
-//     entries.forEach(entry => {
-//       if (entry.isIntersecting) {
-//         startMarquee();
-//       } else {
-//         stopMarquee();
-//       }
-//     });
-//   }, { threshold: 0.5 });
+  let observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          startAnimation();
+        } else {
+          stopAnimation();
+        }
+      });
+    },
+    { threshold: 0.5 }
+  );
 
-//   // Встановлення спостерігача
-//   const coversSection = document.querySelector('.covers-section');
-//   observer.observe(coversSection);
-// });
-
-/* JavaScript for viewport detection and animation triggering */
-//  const section = document.querySelector('.covers-section'); // Replace with actual selector
-
-//  const animationObserver = new IntersectionObserver(entries => {
-//    entries.forEach(entry => {
-//      if (entry.isIntersecting) {
-//        section.classList.add('in-viewport'); // Add a class to trigger animation
-//     } else {
-//       section.classList.remove('in-viewport'); // Remove class to stop animation
-//    }
-//   });
-// });
-
-//  animationObserver.observe(section);
-
+  const coversSection = document.querySelector('.covers-section');
+  observer.observe(coversSection);
+});
