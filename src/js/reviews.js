2086722445fc679babe04ecc5f795dd1e37f80b8
LikaDevelop <67103713+LikaDevelop@users.noreply.github.com>
2024-04-15 20:31:52 +0300

    add function

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -9,31 +9,32 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-
-const list_html_review = document.querySelector(".review-list");
-const list_of_reviews = await apiGet();
-for (let i = 0; i < list_of_reviews.length; i++) {
-    const card_of_review = `<li class="review-card swiper-slide">
+export async function review_html() {
+    const list_html_review = document.querySelector(".review-list");
+    const list_of_reviews = await apiGet();
+    for (let i = 0; i < list_of_reviews.length; i++) {
+        const card_of_review = `<li class="review-card swiper-slide">
                             <img src=${list_of_reviews[i].avatar_url} alt="avatar" class="img-avatar">
                             <h3 class="author-name"> ${list_of_reviews[i].author}</h3>
                             <p class="review-text">${list_of_reviews[i].review}</p>
                         </li>`;
-    list_html_review.innerHTML += card_of_review;
+        list_html_review.innerHTML += card_of_review;
+    }
+    const swiper = new Swiper('.swiper', {
+        modules: [Navigation, Pagination, Keyboard],
+
+        navigation: {
+            nextEl: '.swiper-btn-next',
+            prevEl: '.swiper-btn-prev',
+        },
+
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+            pageUpDown: true,
+        },
+
+    });
+
 }
-
-const swiper = new Swiper('.swiper', {
-    modules: [Navigation, Pagination, Keyboard],
-
-    navigation: {
-        nextEl: '.swiper-btn-next',
-        prevEl: '.swiper-btn-prev',
-    },
-
-    keyboard: {
-        enabled: true,
-        onlyInViewport: true,
-        pageUpDown: true,
-    },
-
-});
 
