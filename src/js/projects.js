38468856da6f4578fe01ddf6a3d7f2fc3b45c1ce
KvashaVladyslav <146590289+KvashaVladyslav@users.noreply.github.com>
2024-04-16 18:57:29 +0200

    upp

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,25 +1,36 @@
 // У файлі main.js
 // core version + navigation, pagination modules:
 import Swiper from 'swiper';
-import { Navigation, Pagination, Keyboard } from 'swiper/modules';
+import { Navigation, Pagination, Keyboard, Mousewheel, EffectCreative, EffectCards } from 'swiper/modules';
 // import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 
-const swiper = new Swiper('.swiper', {
-  modules: [Navigation, Pagination, Keyboard],
-  
+const swiper = new Swiper('.project .swiper.project', {
+  slidesPerView: 1,
+  speed: 1200,
+  direction: 'horizontal',
+  spaceBetween: 32,
   navigation: {
-    nextEl: '.swiper-btn-next',
-    prevEl: '.swiper-btn-prev',
+    nextEl: '.project .swiper-btn-next',
+    prevEl: '.project .swiper-btn-prev',
   },
-
+  modules: [Navigation, Keyboard, Mousewheel, EffectCreative, EffectCards, Pagination],
+  mousewheel: {
+    invert: true,
+  },
   keyboard: {
     enabled: true,
     onlyInViewport: true,
-    pageUpDown: true,
+  },
+  effect: 'cards',
+  EffectCards: {
+    perSlideOffset: 8,
+    perSlideRotate: 2,
+    rotate: true,
+    slideShadows: true,
   },
 
 });
--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,22 +1,43 @@
 import { apiGet } from "./api";
 import Swiper from 'swiper';
-import { Navigation, Pagination, Keyboard } from 'swiper/modules';
+import { Navigation, Pagination, Keyboard, Mousewheel } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 const list_html_review = document.querySelector(".review-list");
 apiGet().then(list_of_reviews => {
-    const swiper = new Swiper('.swiper', {
-        modules: [Navigation, Pagination, Keyboard],
+    const swiper = new Swiper('.reviews .swiper.reviews', {
+        modules: [Navigation, Pagination, Keyboard, Mousewheel],
         navigation: {
-            nextEl: '.swiper-btn-next',
-            prevEl: '.swiper-btn-prev',
+            nextEl: '.reviews .swiper-btn-next',
+            prevEl: '.reviews .swiper-btn-prev',
         },
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 16,
         keyboard: {
             enabled: true,
             onlyInViewport: true,
-            pageUpDown: true,
+            pageUpDown: true
         },
+        mousewheel: {
+            enabled: true,
+            invert: true
+        },
+        pagination: true,
+        speed: 1200,
+        simulateTouch: true,
+        updateOnWindowResize: true,
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                slidesPerGroup: 1,
+            },
+            1400: {
+                slidesPerView: 4,
+                slidesPerGroup: 1,
+            }
+        }
     });
     list_html_review.insertAdjacentHTML('beforeend', renderReviews(list_of_reviews));
 })
