656e7da31c32dfe7399b0381b2776097b28b8aa6
KvashaVladyslav <146590289+KvashaVladyslav@users.noreply.github.com>
2024-04-18 19:50:55 +0200

    up

--- src/js/faq.js
+++
@@ -1,7 +0,0 @@
-const accordionList = document.querySelectorAll(".accordion-faq");
-
-accordionList.forEach(item => {
-    item.addEventListener("click", () => {
-        item.classList.toggle("active");
-    });
-})
